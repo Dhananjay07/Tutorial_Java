286b3560e4f7f53db13b248cec5bd69129732aab
Dhananjay07 <53078305+Dhananjay07@users.noreply.github.com>
2023-08-13 09:09:58 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,22 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int value1;  //Declare the vaible in one line
+		value1=1;     // and we intialized the varible on second line
+		int value2=2, value3=3;
+		int value4 = (5*5)+6;
+		int value5 = (5+5)+value4;
+		int value6 = value2*value3;
+		int value7 = value3*value4*value5;
+		System.out.println(value1);
+		System.out.println(value2);
+		System.out.println(value3);
+		System.out.println(value4);
+		System.out.println(value5); 
+		System.out.println(value6);
+		System.out.println(value7);
+
+	} 
+
+}
